d907546402968d16df94261f16ca4bcbbd6ae2f5
Sebastian Tramp <mail@sebastian.tramp.name>
2011-09-15 00:39:17 +0200

    finish environment for suggest action

--- VoidController.php
+++ VoidController.php
@@ -15,6 +15,30 @@
  */
 class VoidController extends OntoWiki_Controller_Component
 {
+    /*
+     * Schema URIs should be created as class constants
+     */
+    const DCTERM_creator = 'http://purl.org/dc/terms/creator';
+    const DCTERM_source  = 'http://purl.org/dc/terms/source';
+    const DCTERM_license = 'http://purl.org/dc/terms/license';
+
+    /*
+     * an array of terms which are tested and HIGHLY suggested
+     * TODO: define more suggestions here
+     */
+    private $suggestedHigh = array(
+        self::DCTERM_creator,
+        self::DCTERM_license
+    );
+
+    /*
+     * an array of terms which are tested and suggested as NICE-TO-HAVE
+     */
+    private $suggestedMedium = array(
+        self::DCTERM_source
+    );
+
+
     /** @var Zend_Controller_Response_Abstract */
     protected $response = null;
 
@@ -26,10 +50,10 @@ class VoidController extends OntoWiki_Controller_Component
         // this provides many default controller vars and other stuff ...
         parent::init();
         // init controller variables
-        $this->store     = $this->_owApp->erfurt->getStore();
+        $this->store    = $this->_owApp->erfurt->getStore();
         $this->config   = $this->_owApp->config;
-        $this->response  = Zend_Controller_Front::getInstance()->getResponse();
-        $this->request   = Zend_Controller_Front::getInstance()->getRequest();
+        $this->response = Zend_Controller_Front::getInstance()->getResponse();
+        $this->request  = Zend_Controller_Front::getInstance()->getRequest();
     }
 
     /**
@@ -74,29 +98,44 @@ class VoidController extends OntoWiki_Controller_Component
         // TODO
 
         // 4. void:triples
-        $sparql = 'TODO';
+        $sparql = <<<'EOT'
+
+EOT;
         $result = $graph->sparqlQuery($sparql);
         // TODO
 
         // 5. void:entities
-        $sparql = 'TODO';
+        $sparql = <<<'EOT'
+
+EOT;
         $result = $graph->sparqlQuery($sparql);
         // TODO
 
         // 6. void:classes
-        $sparql = 'TODO';
+        $sparql = <<<'EOT'
+
+EOT;
         $result = $graph->sparqlQuery($sparql);
         // TODO
 
         // 7. void:properties
-        $sparql = 'TODO';
+        $sparql = <<<'EOT'
+
+EOT;
         $result = $graph->sparqlQuery($sparql);
         // TODO
-        
+
         // Now we add the statements to the store.
         $store->addMultipleStatements($graphUri, $statements);
     }
 
+    /*
+     * suggest action: scans for highly suggested as well as nice to have
+     * attributes which can not be created automatically and suggests the
+     * missing attributes
+     *
+     * @todo add more attributes to scan for (at the beginning of this file)
+     */
     public function suggestAction()
     {
         // this action needs no view
@@ -106,20 +145,70 @@ class VoidController extends OntoWiki_Controller_Component
         $this->_helper->layout()->disableLayout();
 
         // We need the graph URI
-        $graph = OntoWiki::getInstance()->selectedModel;
+        $graph    = OntoWiki::getInstance()->selectedModel;
         $graphUri = $graph->getModelIri();
 
         // We need a reference to the store for SPARQL and adding the statements.
         $store = Erfurt_App::getInstance()->getStore();
 
-        // SPARQL queries go here...
-        $sparql = '';
-        $result = $graph->sparqlQuery($sparql);
-        // TODO
+        // In addition to the store, we fetch the resource description, which
+        // is a more time efficient way to check of direct properties
+        $graphResource = new OntoWiki_Resource($graphUri, $graph);
+        $memModel = new Erfurt_Rdf_MemoryModel($graphResource->getDescription());
 
+        // an array of HIGHLY suggested properties which do NOT exist yet
+        $notFoundHigh = array();
+        // test for the existence of all HIGHLY suggested property
+        foreach ($this->suggestedHigh as $propertyUri) {
+            if (!$memModel->hasSP($graphUri, $propertyUri)) {
+                $notFoundHigh[] = $propertyUri;
+            }
+        }
+
+        // an array of MEDIUM suggested properties which do NOT exist yet
+        $notFoundMedium = array();
+        foreach ($this->suggestedMedium as $propertyUri) {
+            if (!$memModel->hasSP($graphUri, $propertyUri)) {
+                $notFoundMedium[] = $propertyUri;
+            }
+        }
+
+        // enrich the the array with property titles: fill the titlehelper
+        $titleHelper = new OntoWiki_Model_TitleHelper($graph);
+        $titleHelper->addResources(array_merge($notFoundHigh, $notFoundMedium));
+
+        // enrich the the array with property titles: fetch the titles
+        $url = new OntoWiki_Url(
+            array('controller' => 'resource', 'action' => 'properties'),
+            array('r')
+        );
+        foreach ($notFoundHigh as $propertyUri) {
+            $url->setParam('r', $propertyUri);
+            $finalHigh[$propertyUri]          = array();
+            $finalHigh[$propertyUri]['url']   = (string) $url;
+            $finalHigh[$propertyUri]['uri']   = $propertyUri;
+            $finalHigh[$propertyUri]['label'] = $titleHelper->getTitle($propertyUri);
+        }
+        foreach ($notFoundMedium as $propertyUri) {
+            $url->setParam('r', $propertyUri);
+            $finalMedium[$propertyUri]          = array();
+            $finalMedium[$propertyUri]['url']   = (string) $url;
+            $finalMedium[$propertyUri]['uri']   = $propertyUri;
+            $finalMedium[$propertyUri]['label'] = $titleHelper->getTitle($propertyUri);
+        }
+        // build the final output structure
         $suggestions = array();
-        // TODO
+        $suggestions[0]['class']      = 'message error';
+        $suggestions[0]['label']      = 'Highly Recommended Properties';
+        $suggestions[0]['properties'] = $finalHigh;
+        $suggestions[1]['class']      = 'message warning';
+        $suggestions[1]['label']      = 'Suggested Additional Properties';
+        $suggestions[1]['properties'] = $finalMedium;
 
-        return json_encode($suggestions);
+        // send the response
+        $this->response->setHeader('Content-Type', 'application/json');
+        $this->response->setBody(json_encode($suggestions));
+        $this->response->sendResponse();
+        exit;
     }
 }
