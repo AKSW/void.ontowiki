742916a3b8b3d38f2d4d2f0bf121f9c69c62388e
Sebastian Tramp <mail@sebastian.tramp.name>
2011-09-15 20:49:01 +0200

    finish create action, add transaction, move template

--- VoidController.php
+++ VoidController.php
@@ -9,6 +9,7 @@
 /**
  * The main VoID controller
  *
+ * @link       https://github.com/AKSW/void.ontowiki
  * @category   OntoWiki
  * @package    OntoWiki_extensions_components_void
  * @subpackage component
@@ -18,9 +19,19 @@ class VoidController extends OntoWiki_Controller_Component
     /*
      * Schema URIs should be created as class constants
      */
-    const DCTERM_creator = 'http://purl.org/dc/terms/creator';
-    const DCTERM_source  = 'http://purl.org/dc/terms/source';
-    const DCTERM_license = 'http://purl.org/dc/terms/license';
+    const DCTERM_creator        = 'http://purl.org/dc/terms/creator';
+    const DCTERM_source         = 'http://purl.org/dc/terms/source';
+    const DCTERM_license        = 'http://purl.org/dc/terms/license';
+    const VOID                  = 'http://rdfs.org/ns/void#';
+    const VOID_Dataset          = 'http://rdfs.org/ns/void#Dataset';
+    const VOID_triples          = 'http://rdfs.org/ns/void#triples';
+    const VOID_classes          = 'http://rdfs.org/ns/void#classes';
+    const VOID_entities         = 'http://rdfs.org/ns/void#entities';
+    const VOID_distinctObjects  = 'http://rdfs.org/ns/void#distinctObjects';
+    const VOID_distinctSubjects = 'http://rdfs.org/ns/void#distinctSubjects';
+    const VOID_properties       = 'http://rdfs.org/ns/void#properties';
+    const VOID_dataDump         = 'http://rdfs.org/ns/void#dataDump';
+    const VOID_sparqlEndpoint   = 'http://rdfs.org/ns/void#sparqlEndpoint';
 
     /*
      * an array of terms which are tested and HIGHLY suggested
@@ -50,10 +61,24 @@ class VoidController extends OntoWiki_Controller_Component
         // this provides many default controller vars and other stuff ...
         parent::init();
         // init controller variables
-        $this->store    = $this->_owApp->erfurt->getStore();
+        $this->erfurt   = $this->_owApp->erfurt;
+        $this->store    = $this->erfurt->getStore();
         $this->config   = $this->_owApp->config;
         $this->response = Zend_Controller_Front::getInstance()->getResponse();
         $this->request  = Zend_Controller_Front::getInstance()->getRequest();
+
+        // no action needs a view and a layout
+        $this->_helper->viewRenderer->setNoRender();
+        $this->_helper->layout()->disableLayout();
+
+        // We need the graph URI
+        $this->graph    = OntoWiki::getInstance()->selectedModel;
+        $this->graphUri = $this->graph->getModelIri();
+
+        // In addition to the store, we fetch the resource description, which
+        // is a more time efficient way to check of direct properties
+        $this->graphResource = new OntoWiki_Resource($this->graphUri, $this->graph);
+        $this->memModel = new Erfurt_Rdf_MemoryModel($this->graphResource->getDescription());
     }
 
     /**
@@ -61,72 +86,77 @@ class VoidController extends OntoWiki_Controller_Component
      */
     public function createAction()
     {
-        // this action needs no view
-        $this->_helper->viewRenderer->setNoRender();
+        // to keep source code lines short
+        $graphUri = $this->graphUri;
+        // we need to create memory model with the statements to add
+        $newGraph = new Erfurt_Rdf_MemoryModel();
 
-        // disable layout
-        $this->_helper->layout()->disableLayout();
+        // start a versioning transaction
+        // currently no registry for types :-(
+        $versioning = $this->erfurt->getVersioning();
+        $actionSpec                = array();
+        $actionSpec['type']        = 81000;
+        $actionSpec['modeluri']    = (string) $graphUri;
+        $actionSpec['resourceuri'] = (string) $graphUri;
+        $versioning->startAction($actionSpec);
 
-        // First we need to create an array with the statements to be added.
-        // Have a look at here: http://docs.api.talis.com/platform-api/output-types/rdf-json
-        $statements = array();
-
-        // We need the graph URI
-        $graph = OntoWiki::getInstance()->selectedModel;
-        $graphUri = $graph->getModelIri();
-
-        // We need a reference to the store for SPARQL and adding the statements.
-        $store = Erfurt_App::getInstance()->getStore();
-
-        // Now we add the statements to the array
+        // Now we add the statements to the memory model
 
         // 1. Add a statement graph a void:Dataset
-        $statements[$graphUri] = array();
-        $statements[$graphUri][EF_RDF_TYPE] = array();
-        $statements[$graphUri][EF_RDF_TYPE][] = array(
-            'type'  => 'uri',
-            'value' => 'http://rdfs.org/ns/void#Dataset'
-        );
+        $newGraph->addRelation($graphUri, EF_RDF_TYPE, self::VOID_Dataset);
 
         // 2. Add a statement for the dump
-        $baseUri = OntoWiki::getInstance()->urlBase;
-        $dumpUri = $baseUri . '/model/export/?f=rdfxml&m=' . urlencode($graphUri);
-        // TODO
+        $urlBase = $this->_owApp->view->urlBase;
+        $dumpUrl = $urlBase . '/model/export/?f=rdfxml&m=' . urlencode($graphUri);
+        $newGraph->addRelation($graphUri, self::VOID_dataDump, $dumpUrl);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_dataDump, null);
 
         // 3. Add a statement for the SPARQL endpoint
-        $endpointUri = $baseUri . '/service/sparql';
-        // TODO
+        $endpointUrl = $urlBase . '/service/sparql';
+        $newGraph->addRelation($graphUri, self::VOID_sparqlEndpoint, $endpointUrl);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_sparqlEndpoint, null);
 
         // 4. void:triples
-        $sparql = <<<'EOT'
-
-EOT;
-        $result = $graph->sparqlQuery($sparql);
-        // TODO
+        $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '*', false, false);
+        $newGraph->addAttribute($graphUri, self::VOID_triples, $count);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_triples, null);
 
         // 5. void:entities
-        $sparql = <<<'EOT'
-
-EOT;
-        $result = $graph->sparqlQuery($sparql);
-        // TODO
+        $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s a ?o}', '?s', true, false);
+        $newGraph->addAttribute($graphUri, self::VOID_entities, $count);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_entities, null);
 
         // 6. void:classes
-        $sparql = <<<'EOT'
-
-EOT;
-        $result = $graph->sparqlQuery($sparql);
-        // TODO
+        $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s a ?o}', '?o', true, false);
+        $newGraph->addAttribute($graphUri, self::VOID_classes, $count);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_classes, null);
 
         // 7. void:properties
-        $sparql = <<<'EOT'
+        $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '?p', true, false);
+        $newGraph->addAttribute($graphUri, self::VOID_properties, $count);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_properties, null);
 
-EOT;
-        $result = $graph->sparqlQuery($sparql);
-        // TODO
+        // 8. void:distinctSubjects
+        $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '?s', true, false);
+        $newGraph->addAttribute($graphUri, self::VOID_distinctSubjects, $count);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_distinctSubjects, null);
+
+        // 9. void:distinctObjects
+        $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '?o', true, false);
+        $newGraph->addAttribute($graphUri, self::VOID_distinctObjects, $count);
+        $this->graph->deleteMatchingStatements($graphUri, self::VOID_distinctObjects, null);
 
         // Now we add the statements to the store.
-        $store->addMultipleStatements($graphUri, $statements);
+        $statements = $newGraph->getStatements();
+        //var_dump($statements); exit;
+        $this->graph->addMultipleStatements($statements);
+        $versioning->endAction();
+
+        // send the response
+        $this->response->setHeader('Content-Type', 'application/json');
+        $this->response->setBody(json_encode(true));
+        $this->response->sendResponse();
+        exit;
     }
 
     /*
@@ -138,23 +168,10 @@ EOT;
      */
     public function suggestAction()
     {
-        // this action needs no view
-        $this->_helper->viewRenderer->setNoRender();
-
-        // disable layout
-        $this->_helper->layout()->disableLayout();
-
-        // We need the graph URI
-        $graph    = OntoWiki::getInstance()->selectedModel;
-        $graphUri = $graph->getModelIri();
-
-        // We need a reference to the store for SPARQL and adding the statements.
-        $store = Erfurt_App::getInstance()->getStore();
-
-        // In addition to the store, we fetch the resource description, which
-        // is a more time efficient way to check of direct properties
-        $graphResource = new OntoWiki_Resource($graphUri, $graph);
-        $memModel = new Erfurt_Rdf_MemoryModel($graphResource->getDescription());
+        // we now prepare everything at init
+        $memModel = $this->memModel;
+        $graphUri = $this->graphUri;
+        $graph    = $this->graph;
 
         // an array of HIGHLY suggested properties which do NOT exist yet
         $notFoundHigh = array();
