d708d3eeae6287783b516e89972b63ba71e28b4e
Sebastian Tramp <mail@sebastian.tramp.name>
2011-09-16 21:23:09 +0200

    add clean action, move suggestion config to ini, some refactoring

--- VoidController.php
+++ VoidController.php
@@ -17,11 +17,14 @@
 class VoidController extends OntoWiki_Controller_Component
 {
     /*
+     * the suggested properties: an array of categories, which are arrays of 
+     * suggestion objects (stdClass)
+     */
+    private $suggestions = array();
+
+    /*
      * Schema URIs should be created as class constants
      */
-    const DCTERM_creator        = 'http://purl.org/dc/terms/creator';
-    const DCTERM_source         = 'http://purl.org/dc/terms/source';
-    const DCTERM_license        = 'http://purl.org/dc/terms/license';
     const VOID                  = 'http://rdfs.org/ns/void#';
     const VOID_Dataset          = 'http://rdfs.org/ns/void#Dataset';
     const VOID_triples          = 'http://rdfs.org/ns/void#triples';
@@ -33,31 +36,18 @@ class VoidController extends OntoWiki_Controller_Component
     const VOID_dataDump         = 'http://rdfs.org/ns/void#dataDump';
     const VOID_sparqlEndpoint   = 'http://rdfs.org/ns/void#sparqlEndpoint';
 
-    /*
-     * an array of terms which are tested and HIGHLY suggested
-     * TODO: define more suggestions here
-     */
-    private $suggestedHigh = array(
-        self::DCTERM_creator,
-        self::DCTERM_license
-    );
-
-    /*
-     * an array of terms which are tested and suggested as NICE-TO-HAVE
-     */
-    private $suggestedMedium = array(
-        self::DCTERM_source
-    );
-
-
-    /** @var Zend_Controller_Response_Abstract */
-    protected $response = null;
-
     /**
      * Constructor
      */
     public function init()
     {
+        // We need the graph URI
+        $this->graph    = OntoWiki::getInstance()->selectedModel;
+        if (!$this->graph) {
+            // no session or no model selected
+            exit;
+        }
+
         // this provides many default controller vars and other stuff ...
         parent::init();
         // init controller variables
@@ -71,14 +61,58 @@ class VoidController extends OntoWiki_Controller_Component
         $this->_helper->viewRenderer->setNoRender();
         $this->_helper->layout()->disableLayout();
 
-        // We need the graph URI
-        $this->graph    = OntoWiki::getInstance()->selectedModel;
         $this->graphUri = $this->graph->getModelIri();
 
         // In addition to the store, we fetch the resource description, which
         // is a more time efficient way to check of direct properties
         $this->graphResource = new OntoWiki_Resource($this->graphUri, $this->graph);
         $this->memModel = new Erfurt_Rdf_MemoryModel($this->graphResource->getDescription());
+
+        $this->titleHelper = new OntoWiki_Model_TitleHelper($this->graph);
+    }
+
+    /**
+     * clean action
+     */
+    public function cleanAction()
+    {
+        // start a versioning transaction
+        // currently no registry for types :-(
+        $versioning = $this->erfurt->getVersioning();
+        $actionSpec                = array();
+        $actionSpec['type']        = 81001;
+        $actionSpec['modeluri']    = (string) $this->graphUri;
+        $actionSpec['resourceuri'] = (string) $this->graphUri;
+        $versioning->startAction($actionSpec);
+        $this->removeDescription();
+        $versioning->endAction();
+
+        // send the response
+        $this->response->setHeader('Content-Type', 'application/json');
+        $this->response->setBody(json_encode('success'));
+        $this->response->sendResponse();
+        exit;
+    }
+
+    /*
+     * removes the automatically created void properties from the resource
+     */
+    private function removeDescription()
+    {
+        // to keep source code lines short
+        $graphUri = $this->graphUri;
+        $graph    = $this->graph;
+
+        // Now we remove all the auto-generated void statements
+        $graph->deleteStatement($graphUri, EF_RDF_TYPE, array('value' => self::VOID_Dataset, 'type' => 'uri'));
+        $graph->deleteMatchingStatements($graphUri, self::VOID_dataDump, null);
+        $graph->deleteMatchingStatements($graphUri, self::VOID_sparqlEndpoint, null);
+        $graph->deleteMatchingStatements($graphUri, self::VOID_triples, null);
+        $graph->deleteMatchingStatements($graphUri, self::VOID_entities, null);
+        $graph->deleteMatchingStatements($graphUri, self::VOID_classes, null);
+        $graph->deleteMatchingStatements($graphUri, self::VOID_properties, null);
+        $graph->deleteMatchingStatements($graphUri, self::VOID_distinctSubjects, null);
+        $graph->deleteMatchingStatements($graphUri, self::VOID_distinctObjects, null);
     }
 
     /**
@@ -109,44 +143,38 @@ class VoidController extends OntoWiki_Controller_Component
         $urlBase = $this->_owApp->view->urlBase;
         $dumpUrl = $urlBase . '/model/export/?f=rdfxml&m=' . urlencode($graphUri);
         $newGraph->addRelation($graphUri, self::VOID_dataDump, $dumpUrl);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_dataDump, null);
 
         // 3. Add a statement for the SPARQL endpoint
         $endpointUrl = $urlBase . '/service/sparql';
         $newGraph->addRelation($graphUri, self::VOID_sparqlEndpoint, $endpointUrl);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_sparqlEndpoint, null);
 
         // 4. void:triples
         $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '*', false, false);
         $newGraph->addAttribute($graphUri, self::VOID_triples, $count);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_triples, null);
 
         // 5. void:entities
         $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s a ?o}', '?s', true, false);
         $newGraph->addAttribute($graphUri, self::VOID_entities, $count);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_entities, null);
 
         // 6. void:classes
         $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s a ?o}', '?o', true, false);
         $newGraph->addAttribute($graphUri, self::VOID_classes, $count);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_classes, null);
 
         // 7. void:properties
         $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '?p', true, false);
         $newGraph->addAttribute($graphUri, self::VOID_properties, $count);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_properties, null);
 
         // 8. void:distinctSubjects
         $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '?s', true, false);
         $newGraph->addAttribute($graphUri, self::VOID_distinctSubjects, $count);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_distinctSubjects, null);
 
         // 9. void:distinctObjects
         $count = $this->store->countWhereMatches($this->graphUri, 'WHERE{?s ?p ?o}', '?o', true, false);
         $newGraph->addAttribute($graphUri, self::VOID_distinctObjects, $count);
-        $this->graph->deleteMatchingStatements($graphUri, self::VOID_distinctObjects, null);
 
-        // Now we add the statements to the store.
+        // first we remove the old description
+        $this->removeDescription();
+        // Now we add the new statements to the store.
         $statements = $newGraph->getStatements();
         $this->graph->addMultipleStatements($statements);
         $versioning->endAction();
@@ -159,6 +187,59 @@ class VoidController extends OntoWiki_Controller_Component
     }
 
     /*
+     * this method maintains the output structure for the suggestAction
+     */
+    private function addSuggestion($propertyUri, $categoryName)
+    {
+        // get the suggestion category = an array of stdClass objects
+        if ( (!isset($this->suggestions[$categoryName])) || (!is_array($this->suggestions[$categoryName])) ) {
+            $this->suggestions[$categoryName] = array();
+        }
+        $category = $this->suggestions[$categoryName];
+
+        // prepare a link to the properties view
+        $url = new OntoWiki_Url(
+            array('controller' => 'resource', 'action' => 'properties'),
+            array('r')
+        );
+        $url->setParam('r', $propertyUri);
+
+        // create the properties object
+        $property               = new stdClass();
+        $property->uri          = $propertyUri;
+        $property->url          = (string) $url;
+
+        // add the property object to the category and the category back to the 
+        // suggestions array
+        $category[$propertyUri]           = $property;
+        $this->suggestions[$categoryName] = $category;
+
+        // pre-fill the titleHelper (used in getSuggestions)
+        $this->titleHelper->addResource($propertyUri);
+    }
+
+    /*
+     * finalizes the suggestAction output and retuns a json_encoded string
+     */
+    private function getSuggestions()
+    {
+        // fetch all these titles from the helper
+        $suggestions = $this->suggestions;
+        foreach ($suggestions as $categoryName => $category) {
+            foreach ($category as $propertyUri => $property) {
+                $label = $this->titleHelper->getTitle($propertyUri);
+                $suggestions[$categoryName][$propertyUri]->label = $label;
+            }
+        }
+
+        // return structure consists of categories and suggestions
+        $return = array();
+        $return['categories'] = $this->_privateConfig->categories->toArray();
+        $return['suggestions'] = $suggestions;
+        return $return;
+    }
+
+    /*
      * suggest action: scans for highly suggested as well as nice to have
      * attributes which can not be created automatically and suggests the
      * missing attributes
@@ -167,63 +248,47 @@ class VoidController extends OntoWiki_Controller_Component
      */
     public function suggestAction()
     {
-        // we now prepare everything at init
-        $memModel = $this->memModel;
-        $graphUri = $this->graphUri;
-        $graph    = $this->graph;
+        $memModel    = $this->memModel;
+        $graphUri    = $this->graphUri;
+        $graph       = $this->graph;
+        $suggestions = $this->_privateConfig->suggestions;
 
-        // an array of HIGHLY suggested properties which do NOT exist yet
-        $notFoundHigh = array();
-        // test for the existence of all HIGHLY suggested property
-        foreach ($this->suggestedHigh as $propertyUri) {
-            if (!$memModel->hasSP($graphUri, $propertyUri)) {
-                $notFoundHigh[] = $propertyUri;
+        // a suggestion should have a preferred and a category,
+        // and optional alternatives
+        foreach ($suggestions as $key => $s) {
+            // skip incorrect config
+            if (!isset($s->preferred)) {
+                continue;
             }
-        }
 
-        // an array of MEDIUM suggested properties which do NOT exist yet
-        $notFoundMedium = array();
-        foreach ($this->suggestedMedium as $propertyUri) {
-            if (!$memModel->hasSP($graphUri, $propertyUri)) {
-                $notFoundMedium[] = $propertyUri;
+            // test for the preferred property first
+            if (!$memModel->hasSP($graphUri, $s->preferred)) {
+                // preferred property is not available, so try alternatives
+                $foundAlternative = false;
+                // go through alternatives and check them too
+                if ( (isset($s->alternatives)) && (is_array($s->alternatives)) ) {
+                    foreach ($s->alternatives as $alternative) {
+                        if ($memModel->hasSP($graphUri, $alternative)) {
+                            $foundAlternative = true;
+                            // skip rest if an alternative was found
+                            break;
+                        }
+                    }
+                }
+                // check if we found an alternative property
+                if ($foundAlternative === false) {
+                    // even no alternative was found, so suggest the preferred 
+                    // property for this case
+                    $this->addSuggestion($s->preferred, $s->category);
+                }
+                // do nothing if an alternative was found
             }
+            // do nothing if the preferred property was found
         }
-
-        // enrich the the array with property titles: fill the titlehelper
-        $titleHelper = new OntoWiki_Model_TitleHelper($graph);
-        $titleHelper->addResources(array_merge($notFoundHigh, $notFoundMedium));
-
-        // enrich the the array with property titles: fetch the titles
-        $url = new OntoWiki_Url(
-            array('controller' => 'resource', 'action' => 'properties'),
-            array('r')
-        );
-        foreach ($notFoundHigh as $propertyUri) {
-            $url->setParam('r', $propertyUri);
-            $finalHigh[$propertyUri]          = array();
-            $finalHigh[$propertyUri]['url']   = (string) $url;
-            $finalHigh[$propertyUri]['uri']   = $propertyUri;
-            $finalHigh[$propertyUri]['label'] = $titleHelper->getTitle($propertyUri);
-        }
-        foreach ($notFoundMedium as $propertyUri) {
-            $url->setParam('r', $propertyUri);
-            $finalMedium[$propertyUri]          = array();
-            $finalMedium[$propertyUri]['url']   = (string) $url;
-            $finalMedium[$propertyUri]['uri']   = $propertyUri;
-            $finalMedium[$propertyUri]['label'] = $titleHelper->getTitle($propertyUri);
-        }
-        // build the final output structure
-        $suggestions = array();
-        $suggestions[0]['class']      = 'message error';
-        $suggestions[0]['label']      = 'Highly Recommended Properties';
-        $suggestions[0]['properties'] = $finalHigh;
-        $suggestions[1]['class']      = 'message warning';
-        $suggestions[1]['label']      = 'Suggested Additional Properties';
-        $suggestions[1]['properties'] = $finalMedium;
 
         // send the response
         $this->response->setHeader('Content-Type', 'application/json');
-        $this->response->setBody(json_encode($suggestions));
+        $this->response->setBody(json_encode($this->getSuggestions()));
         $this->response->sendResponse();
         exit;
     }
